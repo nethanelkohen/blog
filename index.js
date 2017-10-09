4d000e644272f5511d5fef27c7aea7670d948887
Nethanel Kohen <nethanel.kohen@gmail.com>
2017-10-09 11:08:08 -0400

    heroku psql fix

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ var bodyParser = require('body-parser');
 var Posts = require("./models/models.js");
 var sql = require("./util/sql.js");
 
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
 app.set('port', (process.env.PORT || 5000));
 
 app.set('view engine', 'pug');
