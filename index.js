a5db7bfce06e24b226b8440f08d07b6fd5dc05e9
Nethanel Kohen <nethanel.kohen@gmail.com>
2017-10-11 17:53:19 -0400

    added heroku pg code

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var app = express();
-var pg = require('pg');
+// var pg = require('pg');
 var bodyParser = require('body-parser');
 var Posts = require("./models/models.js");
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname));
