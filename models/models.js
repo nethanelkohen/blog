407fa6c4ae6ae0ef4a0ec8d014361cf6ea18209f
Nethanel Kohen <nethanel.kohen@gmail.com>
2017-10-11 18:26:31 -0400

    heroku fix again

--- models/models.js
+++ models/models.js
@@ -1,12 +1,25 @@
 var Sequelize = require("sequelize");
 
-var sequelize = new Sequelize({
-  username: 'nethanelkohen',
-  dialect: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  database: 'blog'
-});
+// var sequelize = new Sequelize({
+//   username: 'nethanelkohen',
+//   dialect: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   database: 'blog'
+// });
+
+if (process.env.DATABASE_URL) {
+  var sequelize = new Sequelize(process.env.DATABASE_URL);
+} else {
+  var sequelize = new Sequelize({
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+  });
+}
 
 var Posts = sequelize.define('post', {
   title: {
